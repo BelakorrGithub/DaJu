83f89c5478784553ee804b7d83f0ed42504ece50
Daniel <dlopez@magicseaweed.com>
2017-01-30 19:45:32 +0000

    Cleaning a lot of code(might stop working)

--- js/main.js
+++ js/main.js
@@ -1,83 +1,68 @@
-'use strict';
+$(function() {
+    var width = 50,
+        length = 50,
+        canvas = document.getElementById('game_board'),
+        context = canvas.getContext('2d'),
+        url = 'https://daju.herokuapp.com/',
+        socket = io(url),
+        myId;
 
-(function() {
+    socket.on('connect', function() {
+        myId = socket.id;
+    });
 
-
-    var WIDTH = 50;
-    var LENGTH = 50;
-
-    var canvas = document.getElementById('game_board');
-    var context = canvas.getContext('2d');
-
-    var URL = "https://daju.herokuapp.com/";
-
-    //var URL = "http://localhost:3000/";
-
-    var socket = io(URL);
-    var my_id = null;
-
-    socket.on("connect",function(){
-        my_id = socket.id;
-    })
-
-
-    socket.on("new user",function(new_player, players){
-        // make same announcment
-        updateSquares(players);
-    })
-
-    socket.on("delete user",function(players){
-        // make same announcment
-        updateSquares(players);
-    })
-
-
-    socket.on('moving', function(players){
+    socket.on('new user', function(new_player, players) {
         updateSquares(players);
     });
 
+    socket.on('delete user', function(players) {
+        updateSquares(players);
+    });
+
+    socket.on('moving', function(players) {
+        updateSquares(players);
+    });
 
     $(window).keydown(function(event){
+        var movement = {
+                socket_id: myId,
+                vel_x: 0,
+                vel_y: 0
+            };
 
-        var movement ={
-            socket_id: my_id,
-            vel_x:0,
-            vel_y:0,
-        }
-        if(event.which == 39){ //right
-            console.log("right ->")
+        if (event.which === 39) { //right
+            console.log('right ->');
             movement.vel_x = 20;
-            socket.emit('moving',movement)
+            socket.emit('moving', movement);
         }
-        if(event.which == 37){ //left
-            console.log("<- left")
+
+        if (event.which === 37) { //left
+            console.log('<- left');
             movement.vel_x = -20;
-            socket.emit('moving',movement)
+            socket.emit('moving', movement);
         }
-        if(event.which == 38){ //up
-            console.log("up ^")
+
+        if (event.which === 38) { //up
+            console.log('up ^');
             movement.vel_y = -20;
-            socket.emit('moving',movement)
+            socket.emit('moving', movement);
         }
-        if(event.which == 40){ //down
-            console.log("down \\/")
+
+        if (event.which === 40) { //down
+            console.log('down \\/');
             movement.vel_y = 20;
-            socket.emit('moving',movement)
+            socket.emit('moving', movement);
         }
+    });
 
-    })
+    function updateSquares(players) {
+        context.clearRect(0,0,canvas.width,canvas.height);
 
-    function updateSquares(players){
-        context.clearRect(0,0,canvas.width,canvas.height)
-        $.each(players,function(index,player){
-
+        $.each(players, function(i, player) {
             context.beginPath();
-            context.rect(player.pos.x, player.pos.y, WIDTH, LENGTH);
+            context.rect(player.pos.x, player.pos.y, width, length);
             context.stroke();
             context.closePath();
-        })
+        });
     }
-
-
-
-})();
+});
