fd98db3539a5f0eae4abdcd29bedf4480262f9cf
Juan <fernandez.alba.juan@gmail.com>
2017-02-02 17:03:01 +0100

    diagonal is now possible. Moving 10 by 10 pixels. Diagonal not working as desired: after a keyup, the key you already had pressed does not fire an event anymore, apparently it is expected: http://stackoverflow.com/questions/5841853/keydown-repetition-breaks-when-keyup-event-for-another-key-is-fired

--- js/main.js
+++ js/main.js
@@ -10,6 +10,14 @@ $(function() {
         myId,
         playerColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 
+
+    var keysPressed = {
+        "39":false,
+        "37":false,
+        "38":false,
+        "49":false,
+    };
+
     socket.on('connect', function() {
         myId = socket.id;
     });
@@ -26,37 +34,38 @@ $(function() {
         updateSquares(players);
     });
 
+    $(window).keyup(function(event){
+        if(event.which === 39 || event.which === 37 || event.which === 38 || event.which === 40) {
+            //movement.vel_x = event.which ===39 ? 20:(event.which ===37 ? 20:0);
+            keysPressed[event.which]=false;
+            //socket.emit('moving', movement);
+        }
+    })
+
+
     $(window).keydown(function(event){
         var movement = {
                 socket_id: myId,
                 vel_x: 0,
                 vel_y: 0
             };
-
-        if (event.which === 39) { //right
-            console.log('right ->');
-            movement.vel_x = 20;
-            socket.emit('moving', movement);
-        }
-
-        if (event.which === 37) { //left
-            console.log('<- left');
-            movement.vel_x = -20;
-            socket.emit('moving', movement);
-        }
-
-        if (event.which === 38) { //up
-            console.log('up ^');
-            movement.vel_y = -20;
-            socket.emit('moving', movement);
-        }
-
-        if (event.which === 40) { //down
-            console.log('down \\/');
-            movement.vel_y = 20;
-            socket.emit('moving', movement);
+            // right               left                    up                  down
+        if(event.which === 39 || event.which === 37 || event.which === 38 || event.which === 40) {
+            keysPressed[event.which]=true;
+            moveSquare();
         }
     });
+
+    function moveSquare(){
+        var movement = {
+                socket_id: myId,
+                vel_x: 0,
+                vel_y: 0
+        };
+        movement.vel_x = keysPressed['39'] ? (keysPressed['37'] ? 0:5):(keysPressed['37'] ? -5:0);
+        movement.vel_y = keysPressed['38'] ? (keysPressed['40'] ? 0:-5):(keysPressed['40'] ? 5:0);
+        socket.emit('moving', movement);
+    }
 
     function updateSquares(players) {
         context.clearRect(0,0,canvas.width,canvas.height);
